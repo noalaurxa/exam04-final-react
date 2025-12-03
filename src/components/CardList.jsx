139934fcdd59bb4d5ed3867017bcc46cea98be62
noalaurxa <naudy.noa@tecsup.edu.pe>
2025-12-03 13:59:21 -0500

    agregar cardList

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,5 +1,13 @@
 import Card from "./Card";
 
 export default function CardList({ data = [] }) {
-    
+  return (
+    <div className="row">
+      {data.map((item) => (
+        <div className="col-md-4 mb-4" key={item.id}>
+          <Card item={item} />
+        </div>
+      ))}
+    </div>
+  );
 }
